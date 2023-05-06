a184823cf4d65bdca5abf0d27c829040ba5aeb16
ral7ex <98921679+ral7ex@users.noreply.github.com>
2023-05-06 12:55:24 -0400

    moved decrypt to a different file

--- Encryption.asm
+++ Encryption.asm
@@ -122,15 +122,6 @@ addi $t4, $t4, 16 #get starting address for next block
 
 slt $t8, $t4, $a2
 bnez $t8, load # if next address to load is less than the ending address, go load and encrypt four more words.
-jr $ra #return when thee specified memory has been encrypted
-
-decrypt: # undoes the encryption for the memory between the addresses stored in $a1 and $a2 using keys in $a3 and $a4
-#@param $a1 the starting address in memory that we want to encrypt
-#@param $a2 the ending address in memory that we want to encrypt
-#@param $a3 the key dictating the addition/subtraction portion of the encrytion algorithm
-#@param $a4 the key dictating the shifts/swaps portion of the encryption algorithm
-#***Note that $a1 and $a2 must be the same starting and ending address as when encrypted otherwise the algorithm will NOT work 
-#as intended. In fact, performing multiple encryption operations (encryption on already encrypted data) is a way to 
-#strengthen the encryption.
+jr $ra #return when the specified memory has been encrypted
 
 
